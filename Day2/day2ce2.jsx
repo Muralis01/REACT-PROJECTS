ccc36aa13fa8bc7f69e2ff56fc634639e0bb5724
MURALISAN J <147301684+Muralis01@users.noreply.github.com>
2024-08-06 13:50:29 +0530

    Add files via upload

---
+++ Day2/day2ce2.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = ()=> {
+    return (
+       <>
+      <h1 style={{color:'green'}}>wdfghjkl;</h1>
+      <p style={{padding:'10px',}}></p>
+      </>
+    );
+  }
+  
+  export default App;
